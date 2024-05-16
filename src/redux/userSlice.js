927eaaad937723b972c3b360036dd262e8b9ea7b
Aman <kamanraj2812@gmail.com>
2024-05-17 01:53:51 +0530

    userSlice change

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -5,13 +5,13 @@ const userSlice = createSlice({
   initialState: null,
   reducers: {
     addUser: (state, action) => {
-        return action.payload;
+      return action.payload;
     },
-    removeUser: (state, action) => {
-        return null;
+    removeUser: () => {
+      return null;
     },
   },
 });
 
-export const  {addUser, removeUser} = userSlice.actions;
+export const { addUser, removeUser } = userSlice.actions;
 export default userSlice.reducer;
